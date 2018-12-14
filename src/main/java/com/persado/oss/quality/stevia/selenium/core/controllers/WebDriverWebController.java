98e455bf0639d1a8726dbbdfbf7cb7e4614358ac
Manos Kousteris <mbkousteris@hotmail.com>
2018-12-14 02:49:51 +0200

    Replace clearing of input with shortcut in WebDriverWebController

--- src/main/java/com/persado/oss/quality/stevia/selenium/core/controllers/WebDriverWebController.java
+++ src/main/java/com/persado/oss/quality/stevia/selenium/core/controllers/WebDriverWebController.java
@@ -7,21 +7,21 @@ package com.persado.oss.quality.stevia.selenium.core.controllers;
  * Copyright (C) 2013 - 2014 Persado
  * %%
  * Copyright (c) Persado Intellectual Property Limited. All rights reserved.
- *  
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *  
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
- *  
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- *  
+ *
  * * Neither the name of the Persado Intellectual Property Limited nor the names
  * of its contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
- *  
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -373,7 +373,18 @@ public class WebDriverWebController extends WebControllerBase implements WebCont
     public void input(String locator, String value) {
         moveToElement(locator);
         WebElement element = waitForElement(locator);
-        element.clear();
+        getFocus(locator);
+        try{
+            executeJavascript("$(\"" + locator.replace("css=","") + "\").select()");
+        }
+        catch(Exception e){
+            try{
+                executeJavascript("document.querySelector(\"" + locator.replace("css=","") + "\").select()");
+            }
+            catch(Exception f){
+                info("Warning input may not be overwriten if already exists");
+            }
+        }
         element.sendKeys(value);
         if (!value.isEmpty()) {
             info("Value '" + value + "' was typed in element with locator '" + locator + "'");
